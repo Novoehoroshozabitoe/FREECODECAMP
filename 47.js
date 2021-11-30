9c2d2041ae1c746939211b923080b16bc0c35eaf
Omyrbek Dosmatov <omyrbekdosmatov@gmail.com>
2021-11-30 18:41:41 +0600

    30.11.21 commit

---
+++ 47.js
@@ -0,0 +1,11 @@
+function randomFraction() {
+  let a = 0;
+    // Only change code below this line
+    if(a === 0){
+       return Math.random();
+    }
+  
+    return 0;
+  
+    // Only change code above this line
+  }
